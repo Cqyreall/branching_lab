1c0b71043ded2f05d2ba9abfaa3ba40667bf06c3
Cyril Ishabiyi <cyrilishabiyi@cyrils-air.home>
2020-09-10 16:53:32 +0100

    Compound Interest MVP done

--- compound_interest/src/compound_interest.py
+++ compound_interest/src/compound_interest.py
@@ -1,2 +1,12 @@
 class CompoundInterest:
+
+    def __init__(self, principal, rate, years):
+        self.principal = principal
+        self.rate = rate/100
+        self.years = years
+        self.number_of_times_per_year = 12
+    
+    def compound_interest_calculated(self):
+        compound_interest = self.principal * (1 + self.rate/12) ** (12 * self.years)
+        return round(compound_interest, 2)
     pass
--- compound_interest/tests/compound_interest_test.py
+++ compound_interest/tests/compound_interest_test.py
@@ -8,13 +8,32 @@ class CompoundInterestTest(unittest.TestCase):
 
     # Should return 732.81 given 100 principal, 10 percent, 20 years
 
+    def test_value_returned_is_same(self):
+        compound_interest = CompoundInterest(100, 10, 20)
+        self.assertEqual(732.81, CompoundInterest.compound_interest_calculated(compound_interest))
+
     # Should return 181.94 given 100 principal, 6 percent, 10 years
+    def test_value_returned_is_equal_to_above(self):
+        compound_interest = CompoundInterest(100, 6, 10)
+        self.assertEqual(181.94, CompoundInterest.compound_interest_calculated(compound_interest))
 
     # Should return 149,058.55 given 100000 principal, 5 percent, 8 years
+    def test_value_returned_is_equal_to_above_2(self):
+        compound_interest = CompoundInterest(100000, 5, 8)
+        self.assertEqual(149058.55, CompoundInterest.compound_interest_calculated(compound_interest))
+
 
     # Should return 0.00 given 0 principal, 10 percent, 1 year
+    def test_value_returned_is_equal_to_above_3(self):
+        compound_interest = CompoundInterest(0, 10, 1)
+        self.assertEqual(0.00, CompoundInterest.compound_interest_calculated(compound_interest))
+
 
     # Should return 100.00 given 100 principal, 0 percent, 10 years
+    def test_value_returned_is_equal_to_above_4(self):
+        compound_interest = CompoundInterest(100, 0, 10)
+        self.assertEqual(100.00, CompoundInterest.compound_interest_calculated(compound_interest))
+
 
 
     # Extention tests
